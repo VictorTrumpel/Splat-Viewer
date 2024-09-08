61dae0572d18f6f8ac853f43b45bff7f95e0cb46
Victor Trumpel <victor.trumpel@gmail.com>
2024-09-08 14:53:36 +0300

    test:написал тест на кнопку возвращения домой

--- src/features/ReturnHomeBtn/view/ReturnHomeBtn.test.tsx
+++ src/features/ReturnHomeBtn/view/ReturnHomeBtn.test.tsx
@@ -1,7 +1,22 @@
-import { describe, test } from "vitest";
-
-
+import { describe, test, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { ReturnHomeBtn } from "./ReturnHomeBtn";
+import userEvent from "@testing-library/user-event";
 
 describe("Спецификация компонента <ReturnHomeBtn />", () => {
-  test("");
+  test("При нажатии на кнопку вызывается handleReturnToHomePage из вью-модел", async () => {
+    const handleReturnToHomePage = vi.fn();
+
+    const useViewModel = () => ({
+      handleReturnToHomePage,
+    });
+
+    render(<ReturnHomeBtn useViewModel={useViewModel} />);
+
+    const button = screen.getByTestId("bind-key-button");
+
+    await userEvent.click(button);
+
+    expect(handleReturnToHomePage).toBeCalledTimes(1);
+  });
 });
