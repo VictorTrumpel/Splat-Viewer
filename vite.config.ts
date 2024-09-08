106268f6fa2050c6899a88c5df65d911c3a3f573
Victor Trumpel <victor.trumpel@gmail.com>
2024-09-08 19:05:32 +0300

    feat:исправил конфиг вита

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name][extname]",
+        chunkFileNames: "[name].js",
+        entryFileNames: "[name].js",
+      },
+    },
+    outDir: "dist", // Устанавливаем папку для выходных файлов
+  },
   plugins: [
     react(),
     checker({
