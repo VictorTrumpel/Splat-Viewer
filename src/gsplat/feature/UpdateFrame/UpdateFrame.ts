efea0e6c3aa5e181c474802b576ce115d7671331
Victor Trumpel <victor.trumpel@gmail.com>
2024-09-09 10:15:07 +0300

    refactor:удалил ненужную фичу из модуля gsplat

--- src/gsplat/feature/UpdateFrame/UpdateFrame.ts
+++
@@ -1,9 +0,0 @@
-import { OrbitControls, Renderer, Scene, Camera } from "@gsplat/core";
-
-export const UpdateFrame = () => {
-  const scene = Scene.get();
-  const camera = Camera.get();
-
-  OrbitControls.update();
-  Renderer.render(scene, camera);
-};
--- src/gsplat/feature/UpdateFrame/index.ts
+++
@@ -1,1 +0,0 @@
-export { UpdateFrame } from "./UpdateFrame";
